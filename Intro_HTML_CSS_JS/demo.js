6debcc3a3e5ee944f2a58581b74cc4752ff2adab
Zia Babar <zbabar@gmail.com>
2020-09-05 15:58:09 -0400

    final version

--- Intro_HTML_CSS_JS/demo.js
+++ Intro_HTML_CSS_JS/demo.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function validInput() {
     var x, text;
   
     // Get the value of the input field with id="numb"
@@ -11,4 +11,32 @@ function myFunction() {
       text = "Input OK";
     }
     document.getElementById("demo").innerHTML = text;
+}
+
+
+function showAlert() {
+}
+    
+function randomInteger(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+    
+function checkRegistration(){
+    var regNumber = randomInteger(1, 100);
+
+    var firstName = document.getElementById('fname').value;
+    var lastName = document.getElementById('lname').value;
+
+    if ((firstName === "") || (lastName === "")) {
+        alert('Please provide a valid first name and last name');
+        return;
+    }
+
+    if (regNumber <= 40) {
+      alert('Thank you for early registration, ' + firstName + ' ' + lastName);
+    } else {
+        alert('Sorry, registration is now closed');
+    }
+
+    
 }
